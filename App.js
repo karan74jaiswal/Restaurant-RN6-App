65b647b174491dbda30cca276499ee6a81083fe2
karan74jaiswal <karan74jaiswal@gmail.com>
2023-10-07 15:10:16 +0530

    Assembled a V6 Stack Navigator

--- App.js
+++ App.js
@@ -1,1 +1,13 @@
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import SearchScreen from "./src/screens/SearchScreen";
+export default function App() {
+  const Stack = createStackNavigator();
+  <NavigationContainer>
+    <Stack.Navigator
+      screenOptions={{ title: "Business Search", headerTitleAlign: "center" }}
+    >
+      <Stack.Screen name="Search" component={SearchScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>;
+}
---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, StyleSheet, View, TextInput } from "react-native";
+
+const SearchScreen = () => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
